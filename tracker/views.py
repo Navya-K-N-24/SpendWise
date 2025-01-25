f9b588be1f2ca63eef2033bba6e5941f467bcc5e
Navya Keelara <navyakn31@gmail.com>
2025-01-25 01:51:50 +0000

    Fix the responsivness

--- tracker/views.py
+++ tracker/views.py
@@ -1,19 +1,16 @@
 from django.shortcuts import render, redirect
-# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-# from django.http import HttpResponse
 from .forms import CustomUserCreationForm
 
-# Create your views here.
-# def home(request):
-   # return HttpResponse("Welcome to SpendWise!")
-
+# Home page view
 def home(request):
-    return render(request, "tracker/index.html")  
+    return render(request, "tracker/index.html")
 
+# About page view
 def about(request):
     return render(request, "tracker/about.html")
 
+# User registration view
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
@@ -24,18 +21,3 @@ def register(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'tracker/register.html', {'form': form})
-
-def login_view(request):
-    # Login logic (assuming it exists)
-    return render(request, 'tracker/login.html')
-
-#def register(request):
- #   if request.method == 'POST':
-  #      form = UserCreationForm(request.POST)
-   #     if form.is_valid():
-    #        form.save()
-     #       messages.success(request, 'Account created successfully! You can now log in.')
-      #      return redirect('login')  # Replace 'login' with the URL name for the login page
-    # else:
-      #  form = UserCreationForm()
-    # return render(request, 'tracker/register.html', {'form': form})
